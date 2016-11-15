1dd3ef3a6d098398c002f5fa6bc34dc48c3c4f59
Robin <a339546210@163.com>
2016-11-16 00:59:04 +0800

    add a !

--- MyProject1/src/HelloWorld2.java
+++ MyProject1/src/HelloWorld2.java
@@ -2,6 +2,6 @@ public class HelloWorld2 {
   public static void main(String[]agrs)
     {
       System.out.println("HelloWorld!");
-      System.out.println("My first pushed java program!!");
+      System.out.println("My first pushed java program!!!");
     }
 }
