c9460695aaa62b681af1cbe6d0184cda292cdfe6
Robin <a339546210@163.com>
2016-11-16 00:52:39 +0800

    My first pushed java program

---
+++ MyProject1/src/HelloWorld2.java
@@ -0,0 +1,7 @@
+public class HelloWorld2 {
+  public static void main(String[]agrs)
+    {
+      System.out.println("HelloWorld!");
+      System.out.println("My first pushed java program!!");
+    }
+}
